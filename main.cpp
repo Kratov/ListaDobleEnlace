279d569e38df75c37beac6568cebb35e18efaeb4
Jaime Zamora <james.zmora@hotmail.com>
2018-08-30 11:23:36 -0500

    Agregado insertar despues de.

--- main.cpp
+++ main.cpp
@@ -2,95 +2,120 @@
 
 using namespace std;
 
+enum Insercion
+{
+	InsercionInicio,
+	InsercionFin,
+	InsercionAdelanteDe
+};
+
 struct Nodo
 {
-	int info;
+	int dato;
 	Nodo * ant;
-	Nodo * sig;
+	Nodo * Siguiente;
 };
+
+int menuPrincipal() {
+	int op = 0;
+	cout << "\n	1.Insetar Nodo\n	2.Mostrar(Inicio)\n	3.Mostrar(Fin)\n	0.Salir\n	Seleccion: ";
+	cin >> op;
+	return op;
+}
+
+int menuTipoInsercion() {
+	int op = 0;
+	cout << "\n	1.Inicio\n	2.Fin\n	3.Despues de aux\n Seleccion: ";
+	cin >> op;
+	return op;
+}
 
 void mostrarDesdeCabeza(Nodo *& cab) {
 	Nodo * aux = cab;
 	while(aux != NULL){
-		cout << "\nDato: "<<aux->info;
-		aux = aux->sig;
+		cout << "\nDato: "<<aux->dato;
+		aux = aux->Siguiente;
 	}
 }
 
 void mostrarDesdeCola(Nodo * fin) {
 	Nodo * aux = fin;
 	while (aux != NULL) {
-		cout << "\nDato: " << aux->info;
+		cout << "\nDato: " << aux->dato;
 		aux = aux->ant;
 	}
 }
 
-void insertarNodoCola(Nodo *& cab, Nodo *& fin, Nodo *& aux, int info) {
-	Nodo * nuevo = new Nodo();
-	nuevo->info = info;
-	nuevo->sig = NULL;
-	nuevo->ant = NULL;
-	if (cab == NULL)
-	{
-		cab = nuevo;
-		fin = nuevo;
-		aux = nuevo;
-	}
-	else 
-	{
-		nuevo->ant = fin;
-		fin->sig = nuevo;
-		fin = nuevo;
-	}
-}
+void insertarNodo(Nodo *& cab, Nodo *& fin, Nodo *& aux, Insercion tipoInsercion, int dato) {
+	Nodo *nuevo_nodo = new Nodo();
+	nuevo_nodo->dato = dato;
+	nuevo_nodo->ant = NULL;
+	nuevo_nodo->Siguiente = NULL;
 
-void insertarNodoInicio(Nodo *& cab, Nodo *& fin, Nodo *& aux, int info) {
-	Nodo * nuevo = new Nodo();
-	nuevo->info = info;
-	nuevo->sig = NULL;
-	nuevo->ant = NULL;
-	if (cab == NULL)
-	{
-		cab = nuevo;
-		fin = nuevo;
-		aux = nuevo;
+	if (cab == NULL) {
+		cab = nuevo_nodo;
+		fin = nuevo_nodo;
+		aux = nuevo_nodo;
 	}
-	else
-	{
-		nuevo->sig = cab;
-		cab->ant = nuevo;
-		cab = nuevo;
+	else {
+
+		switch (tipoInsercion)
+		{
+		case InsercionInicio:
+			nuevo_nodo->Siguiente = cab;
+			cab->ant = nuevo_nodo;
+			cab = nuevo_nodo;
+			break;
+		case InsercionFin:
+			fin->Siguiente = nuevo_nodo;
+			nuevo_nodo->ant = fin;
+			fin = nuevo_nodo;
+			break;
+		case InsercionAdelanteDe:
+			nuevo_nodo->Siguiente = aux->Siguiente;
+			nuevo_nodo->ant = aux;
+			if (aux->Siguiente != NULL)
+				aux->Siguiente->ant = nuevo_nodo;
+			aux->Siguiente = nuevo_nodo;
+			break;
+		}
+		aux = cab;
 	}
-}
-
-
-char menuPrincipal()
-{
-	char sel = 'S';
-	printf("\nPILAS Y COLAS\n	1.Insertar\n	2.Eliminar\n	3.Recorrer\n	S.Salir\n	Seleccion: ");
-	scanf(" %c", &sel);
-	return sel;
 }
 
 int main() {
-	Nodo * cab, *fin, *aux;
+	int dato, op = 0;
+	Nodo *cab, *fin, *aux;
 	cab = fin = aux = NULL;
-	int dat;
-	char opc = 'S';
 	do {
 		system("CLS");
-		switch (opc = menuPrincipal())
+		if (aux != NULL)
+			cout << "\nEl auxiliar es: " << aux->dato;
+		switch (op = menuPrincipal())
 		{
-		case '1':
-			
+		case 1:
+			cout << "	\nIngrese el dato a insertar: ";
+			cin >> dato;
+			switch (menuTipoInsercion())
+			{
+			case 1:
+				insertarNodo(cab, fin, aux, InsercionInicio, dato);
+				break;
+			case 2:
+				insertarNodo(cab, fin, aux, InsercionFin, dato);
+				break;
+			case 3:
+				insertarNodo(cab, fin, aux, InsercionAdelanteDe, dato);
+				break;
+			}
 			break;
-		case '2':
-			
+		case 2:
+			mostrarDesdeCabeza(cab);
 			break;
-		case '3':
-			
+		case 3:
+			mostrarDesdeCola(fin);
 			break;
 		}
 		system("PAUSE");
-	} while (opc != 'S');
+	} while (op != 0);
 }
